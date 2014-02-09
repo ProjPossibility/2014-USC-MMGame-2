087e082ee90cf01ce977a023120e6a34ba62b17b
Ryan Choi <ryanchoi@usc.edu>
2014-02-09 00:52:36 -0800

    android app sends server data in json format

--- Battleship/src/info/ss12/battleship/BattleshipApp.java
+++ Battleship/src/info/ss12/battleship/BattleshipApp.java
@@ -29,8 +29,7 @@ import com.google.gson.Gson;
 
 public class BattleshipApp extends Activity {
 
-<<<<<<< HEAD
-	Ship[] ships = new Ship[5];
+	Ship[] Ships = new Ship[5];
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +49,7 @@ public class BattleshipApp extends Activity {
 		tv.setText("hello");
 		Intent myIntent = new Intent(BattleshipApp.this, ScrollPage.class);
 		//myIntent.putExtra("key", value); //Optional parameters
-		//new SendData().execute(); // done
+		new SendData().execute(); // done
 		BattleshipApp.this.startActivity(myIntent);
 	}
 
@@ -72,19 +71,26 @@ public class BattleshipApp extends Activity {
 			Gson gson = new Gson();
 			try{
 				HttpClient httpclient = new DefaultHttpClient();
-				HttpPost httppost = new HttpPost("54.213.43.47");
+				HttpPost httppost = new HttpPost("http://ec2-54-213-43-47.us-west-2.compute.amazonaws.com");
 				// JSON data:
 				RandomBoardGenerator((short)0);
-				gson.toJson(ships);
+				String jsonstr = gson.toJson(Ships);
+				System.out.println(jsonstr);
 				json.put("limit",3); // best of 3
 				json.put("cmd",0); // 0 = start game
+				json.put("dataa",jsonstr);
 				JSONArray postjson = new JSONArray();
 				postjson.put(json);
 				// Post the data:
-				httppost.setHeader("json",json.toString());
+				httppost.setHeader("json",jsonstr);
 				httppost.getParams().setParameter("jsonpost",postjson);
 				// Execute HTTP Post Request
-				HttpResponse response = httpclient.execute(httppost);
+				HttpResponse response = null;
+				try{
+				response = httpclient.execute(httppost);
+				}catch(Exception e){
+					System.out.println(e);
+				}
 				// for reading response (json):
 				if(response != null)
 				{
@@ -107,14 +113,12 @@ public class BattleshipApp extends Activity {
 					}
 					//display in log
 					Log.i("RESPONSE", sb.toString());
-					if(sb.toString().contains("data accepted")){
+					if(sb.toString().contains("yay")){
 						//do something with the string
 						Intent intent = new Intent(BattleshipApp.this, ScrollPage.class);
 						startActivity(intent); //switch activities
 					}else{
-						Toast.makeText(getApplicationContext(), 
-								"The server had trouble with your request. Try again later", 
-								Toast.LENGTH_LONG).show();
+						Toast.makeText(getApplicationContext(), "The server had trouble with your request. Try again later", Toast.LENGTH_LONG).show();
 					}
 
 
@@ -138,21 +142,38 @@ public class BattleshipApp extends Activity {
 	
 	public void RandomBoardGenerator(short team){ // team being 0 or 1
 		//6x6 board with ships of lengths 3, 2, 2, 1, 1.
-		ships[0] = new Ship(team, 3);
-		ships[1] = new Ship(team, 2);
-		ships[2] = new Ship(team, 2);
-		ships[3] = new Ship(team, 1);
-		ships[4] = new Ship(team, 1);
-		ships[0].hitboxes[0] = new Hitbox((short)3, (short)3, team, 0);
+		Ships[0] = new Ship(team, 3, 0);
+		Ships[1] = new Ship(team, 2, 1);
+		Ships[2] = new Ship(team, 2, 2);
+		Ships[3] = new Ship(team, 1, 3);
+		Ships[4] = new Ship(team, 1, 4);
+		
+		Ships[0].hitboxes[0] = new Hitbox((short)0, (short)0, team, 0);
+		Ships[0].hitboxes[1] = new Hitbox((short)0, (short)1, team, 0);
+		Ships[0].hitboxes[2] = new Hitbox((short)0, (short)2, team, 0);
+		
+		Ships[1].hitboxes[0] = new Hitbox((short)3, (short)3, team, 1);
+		Ships[1].hitboxes[1] = new Hitbox((short)3, (short)2, team, 1);
+		
+		Ships[2].hitboxes[0] = new Hitbox((short)2, (short)1, team, 2);
+		Ships[2].hitboxes[1] = new Hitbox((short)2, (short)2, team, 2);
+		
+		Ships[3].hitboxes[0] = new Hitbox((short)5, (short)5, team, 3);
+	
+		Ships[4].hitboxes[0] = new Hitbox((short)1, (short)4, team, 4);
 	}
 	
 	public class Ship{
+		int ship_ID;
 		short team;
 		boolean sunk;
 		Hitbox[] hitboxes;
-		Ship(short a, int b){
+		int num_hitbox;
+		Ship(short a, int b, int c){
 			team = a;
+			num_hitbox = b;
 			hitboxes = new Hitbox[b]; // uninitialized array of hitboxes.
+			ship_ID = c;
 		}
 	}
 	
@@ -160,32 +181,12 @@ public class BattleshipApp extends Activity {
 		short x;
 		short y;
 		short team;
-		int ShipID;
+		int ship_ID;
 		Hitbox(short a, short b, short c, int d){
 			x = a;
 			y = b;
 			team = c;
-			ShipID = d;
+			ship_ID = d;
 		}
 	}
-=======
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public void clicked(View view) {
-    	Intent myIntent = new Intent(BattleshipApp.this, Grid.class);
-    	//myIntent.putExtra("key", value); //Optional parameters 
-    	BattleshipApp.this.startActivity(myIntent);
-    }
->>>>>>> 1c20b27dd69b8095ec6183986bf59d454bae5743
 }
